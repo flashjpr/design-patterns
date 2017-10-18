ee0f19c5703ddb575a9d7cea3f387e60c0b1b0bb
flashjpr <flashjpr@gmail.com>
2017-10-18 22:22:52 +0100

    Example for how programmers tend to bundle the algorithm logic in the owner class

---
+++ strategy/src/encryption/Encryptor.java
@@ -0,0 +1,23 @@
+package encryption;
+
+public class Encryptor {
+
+    private String algorithmName;
+    private String plainText;
+
+    public Encryptor (String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public void encrypt() {
+        if (algorithmName.equals("AES")) {
+            System.out.println("Encrypting the information using AES");
+            // code omitted
+        } else if (algorithmName.equals("MD5")) {
+            System.out.println("Encrypting the information using MD5");
+            // code ommited
+        } // etc etc
+    }
+
+    // getters and setters for plaintext
+}
