6fdf7c0a98e67fe3f9dabadaff3032c767b8dff3
flashjpr@gmail.com <16Kickbox>
2017-10-23 22:23:44 +0100

    Checked the price of an espresso with milk

--- decorator/test/coffeeshop/CoffeShopDecoratorTest.java
+++ decorator/test/coffeeshop/CoffeShopDecoratorTest.java
@@ -1,13 +1,22 @@
+package coffeeshop;
+
+import cofeeshop.AddonDecorator;
 import cofeeshop.Beverage;
 import cofeeshop.Espresso;
+import cofeeshop.Milk;
+import junit.framework.Assert;
 import org.junit.Test;
 
-class CoffeShopDecoratorTest {
+import static org.junit.Assert.*;
+
+public class CoffeShopDecoratorTest {
 
     @Test
     public void testEspressoWithMilkPrice() {
-
         Beverage espresso = new Espresso();
-        System.out.println(espresso.cost());
+        assertEquals("Price of a single espresso is not as expected", 3, espresso.cost());
+
+        AddonDecorator espressoWithMilk = new Milk(new Espresso());
+        assertEquals("Price of an expresso with milk is not as expected", 4, espressoWithMilk.cost());
     }
 }
